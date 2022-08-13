301d555ccc60366d4ce9bce5498d0c2902b22b3a
Ana Paula Berto <annapaulabertho@gmail.com>
2022-08-13 08:48:57 -0300

    update

--- login.php
+++ login.php
@@ -1,16 +1,57 @@
+<?php
+	// session_start();
+
+	// $login = $_POST['usuario'];
+	// $senha = $_POST['senha'];
+
+	// $servername = "localhost";
+	// $username = "ifpb";
+	// $password = "ifpb";
+	// $dbname = "secitec";
+
+	
+	// $conn = new mysqli($servername, $username, $password, $dbname);
+
+	// if ($conn->connect_error) {
+	// 	die("Connection failed: " . $conn->connect_error);
+	// }
+
+	// $sql = "SELECT usuario, senha FROM usuario WHERE usuario = '$login' AND senha = '$senha'";
+	// $result = $conn->query($sql);
+
+	// if ($result->num_rows > 0) {
+
+	// 		$_SESSION['usuario'] = $login;
+	// 		$_SESSION['senha'] = $senha;
+	// 		header('location:areacliente.php');
+			
+	// }
+	// 	else {
+	// 	echo "0 results";
+	// 	unset ($_SESSION['usuario']);
+	// 	unset ($_SESSION['senha']);
+	// 	header('location:login.html');
+	// }
+	// $conn->close();
+
+
+?>
 
 <?php
-	$login = $_POST['usuario'];
+// session_start inicia a sessão
+	session_start();
+	// as variáveis login e senha recebem os dados digitados na página anterior
+	$login = $_POST['login'];
 	$senha = $_POST['senha'];
-
+	// as próximas 3 linhas são responsáveis em se conectar com o bando de dados.
 	$servername = "localhost";
 	$username = "ifpb";
 	$password = "ifpb";
 	$dbname = "secitec";
 
-	// Create connection
+	
 	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
+
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
@@ -32,7 +73,5 @@
 		header('location:login.html');
 	}
 	$conn->close();
-
-
 ?>
 
